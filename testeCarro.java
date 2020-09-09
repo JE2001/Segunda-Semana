d24f525d21331e561d4aa8b93983d505064ab1df
JE2001 <jetnsa2001@gmail.com>
2020-09-09 13:03:13 -0300

    Captura nome e exibe. Exercicio pronto.

--- testeCarro.java
+++
@@ -1,8 +0,0 @@
-public class testeCarro {
-    public static void main(String[] args) {
-        Carros carros1 = new Carros("Azul", "Gol");
-        Carros carros2 = new Carros("Branco", "Ford");
-        carros1.descreverCarro();
-        carros2.descreverCarro();
-    }
-}
